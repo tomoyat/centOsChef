57df33111ec94bc29cae4f417619ad0d0a975046
tomoyat <tomoya@ram-VirtualBox.(none)>
2013-10-09 23:20:03 +0900

    added, base recipe

---
+++ site-cookbooks/base/recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "git" do
+  action :install
+end
+
+
+package "zsh" do
+  action :install
+end
+
+package "tmux" do
+  action :install
+end
+
+package "tree" do
+  action :install
+end
+
+package "vim" do
+  action :install
+end
