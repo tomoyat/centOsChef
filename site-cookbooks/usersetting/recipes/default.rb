2339cd23234673fa8a7abe08e4a4b85df2bdac12
tomoyat <tomoya@ram-VirtualBox.(none)>
2013-10-20 18:00:27 +0900

    add user setting

---
+++ site-cookbooks/usersetting/recipes/default.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: usersetting
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+data_ids = data_bag('users')
+suders = []
+data_ids.each do |id|
+  u = data_bag_item('users',id)
+  if u['sudoer'] == 'yes'
+    suders << u['user_name']
+  end
+end
+
+data_ids.each do |id|
+  u = data_bag_item('users',id)
+  group u['group_name'] do
+    group_name u['group_name']
+    gid u['gid']
+    action [:create]
+  end
+  user u['user_name'] do
+    uid u['uid']
+    home u['home']
+    password u['password']
+    supports :manage_home => true # make hoem directory
+  end
+  group u['group_name'] do
+    group_name u['group_name']
+    gid u['gid']
+    members u['user_name']
+    action [:modify]
+  end
+  directory "#{u['home']}/.ssh" do
+    owner u['user_name']
+    group u['user_name']
+  end
+  authorized_keys_file = "#{u['home']}/.ssh/authorized_keys"
+  file authorized_keys_file do
+    owner u['user_name']
+    group u['user_name']
+    mode 0600
+    content "#{u['ssh_key']}"
+    not_if { ::File.exists?("#{authorized_keys_file}") }
+  end
+end
+
+group 'wheel' do
+  members suders
+  action [:modify]
+end
